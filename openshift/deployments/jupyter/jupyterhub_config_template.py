e1fa4093136785db968c91cb48c82c6b200cc856
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2019-02-18 12:51:52 +0000

    reworked jupyter deployment to use postgres from infra project

--- openshift/deployments/jupyter/jupyterhub_config_template.py
+++ openshift/deployments/jupyter/jupyterhub_config_template.py
@@ -29,16 +29,36 @@ c.KubeSpawner.profile_list = [
 # authentication
 
 import os
-os.environ['OAUTH2_TOKEN_URL'] = 'https://sso.192.168.99.100.nip.io/auth/realms/squonk/protocol/openid-connect/token'
-os.environ['OAUTH2_AUTHORIZE_URL'] = 'https://sso.192.168.99.100.nip.io/auth/realms/squonk/protocol/openid-connect/auth'
-os.environ['OAUTH2_USERDATA_URL'] = 'https://sso.192.168.99.100.nip.io/auth/realms/squonk/protocol/openid-connect/userinfo'
+os.environ['OAUTH2_TOKEN_URL'] = 'https://<keycloak-server>/auth/realms/<realm-name>/protocol/openid-connect/token'
+os.environ['OAUTH2_AUTHORIZE_URL'] = 'https://<keycloak-server>/auth/realms/<realm-name>/protocol/openid-connect/auth'
+os.environ['OAUTH2_USERDATA_URL'] = 'https://<keycloak-server>/auth/realms/<realm-name>/protocol/openid-connect/userinfo'
 os.environ['OAUTH2_TLS_VERIFY'] = '0'
 os.environ['OAUTH2_USERNAME_KEY'] = 'preferred_username'
 
 from oauthenticator.generic import GenericOAuthenticator
 c.JupyterHub.authenticator_class = GenericOAuthenticator
 c.OAuthenticator.client_id = 'jupyterhub'
-c.OAuthenticator.client_secret = 'ae2b5371-792d-4703-9a7c-4ec616d14415'
-c.OAuthenticator.oauth_callback_url = 'https://jupyterhub-jupyter.192.168.99.100.nip.io/hub/oauth_callback'
+c.OAuthenticator.client_secret = '<client-secret>'
+c.OAuthenticator.oauth_callback_url = 'https://<juputerhub-route>/hub/oauth_callback'
 c.OAuthenticator.tls_verify = False
-c.OAuthenticator.login_service = 'Squonk SSO'
+c.OAuthenticator.login_service = 'OpenRiskNet SSO'
+
+
+# storage
+c.KubeSpawner.user_storage_pvc_ensure = True
+c.KubeSpawner.pvc_name_template = 'jupyterhub-nb-{username}'
+c.KubeSpawner.user_storage_capacity = '1Gi'
+c.KubeSpawner.volumes = [
+    {
+        'name': 'data',
+        'persistentVolumeClaim': {
+            'claimName': c.KubeSpawner.pvc_name_template
+        }
+    }
+]
+c.KubeSpawner.volume_mounts = [
+    {
+        'name': 'data',
+        'mountPath': '/opt/app-root/src'
+    }
+]
