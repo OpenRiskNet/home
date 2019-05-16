564538be9390515ac0d906c2c94ed6cc6532ea9e
gebele <gebele@in-silico.ch>
2019-05-16 12:07:56 +0200

    added more parameters

--- openshift/deployments/lazar/deploy.sh
+++ openshift/deployments/lazar/deploy.sh
@@ -5,10 +5,12 @@ source setenv.sh
 
 echo "IMAGE_TAG set to ${IMAGE_TAG}"
 echo "ROUTES_BASENAME set to ${ROUTES_BASENAME}"
+echo "ROUTE_NAME set to ${ROUTE_NAME}"
 echo "LAZAR_SERVICE_PORT set to ${LAZAR_SERVICE_PORT}"
 
 oc process -f lazar.yaml \
   -p IMAGE_TAG=$IMAGE_TAG \
   -p ROUTES_BASENAME=$ROUTES_BASENAME \
+  -p ROUTE_NAME=$ROUTE_NAME \
   -p LAZAR_SERVICE_PORT=$LAZAR_SERVICE_PORT \
 | oc create -f -
