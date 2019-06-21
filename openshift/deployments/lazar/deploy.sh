735167249ad812e25e044dd4812270fa78badaae
gebele <gebele@in-silico.ch>
2019-06-21 11:03:39 +0200

    add TLS

--- openshift/deployments/lazar/deploy.sh
+++ openshift/deployments/lazar/deploy.sh
@@ -7,10 +7,12 @@ echo "IMAGE_TAG set to ${IMAGE_TAG}"
 echo "ROUTES_BASENAME set to ${ROUTES_BASENAME}"
 echo "ROUTE_NAME set to ${ROUTE_NAME}"
 echo "LAZAR_SERVICE_PORT set to ${LAZAR_SERVICE_PORT}"
+echo "TLS set to ${TLS}"
 
 oc process -f lazar.yaml \
   -p IMAGE_TAG=$IMAGE_TAG \
   -p ROUTES_BASENAME=$ROUTES_BASENAME \
   -p ROUTE_NAME=$ROUTE_NAME \
   -p LAZAR_SERVICE_PORT=$LAZAR_SERVICE_PORT \
+  -p TLS=$TLS \
 | oc create -f -
